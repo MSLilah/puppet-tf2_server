639571cae29d58c1980c239b0430997b4e41bf2e
Preben Ingvaldsen <preben@puppetlabs.com>
2014-10-30 16:28:37 -0700

    Add configuration to tf2_server
    Add the ability to configure a tf2_server to the tf2_server
    class.

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,4 +1,10 @@
-class tf2_server {
+class tf2_server (
+  $hostname,
+  $rcon_password,
+  $sv_contact,
+  $map_timelimit,
+  $motd,
+) {
   require tf2_server::steamcmd
   file { '/home/steam/hlds/steamcmd.sh':
     ensure => file,
@@ -12,7 +18,7 @@ class tf2_server {
   }
   file { '/home/steam/hlds/tf2_ds.txt':
     ensure  => file,
-    content => "login anonymous\nforce_install_dir ./tf2\napp_update 232250\nquit\n",
+    content => "login anonymous\nforce_install_dir ./tf2\napp_update 232250 validate\nquit\n",
   }
   file { '/home/steam/hlds/update.sh':
     ensure  => file,
@@ -28,5 +34,41 @@ class tf2_server {
     logoutput => true,
     require => [File['/home/steam/hlds/steamcmd.sh'], File['/home/steam/hlds/linux32/steamcmd'],
                 File['/home/steam/hlds/tf2_ds.txt'], File['/home/steam/hlds/update.sh']],
+    before => [File['/home/steam/hlds/tf2/tf/cfg/server.cfg'], File['/home/steam/hlds/tf2/tf/cfg/motd.txt'],
+               File['/home/steam/hlds/tf2/tf/cfg/maplist.txt'], File['/home/steam/hlds/tf2/tf/cfg/mapcycle.txt'],
+               File['/home/steam/hlds/tf2/tf/cfg/ctf_2fort.cfg'], File['/home/steam/hlds/tf2/steam_appid.txt']],
+  }
+  file { '/home/steam/hlds/tf2/tf/cfg/server.cfg':
+    ensure => file,
+    content => "hostname \"$hostname\"
+                rcon_password \"$rcon_password\"
+                sv_contact \"$sv_contact\"
+                mp_timelimit \"$map_timelimit\"",
+  }
+  file { '/home/steam/hlds/tf2/tf/cfg/motd.txt':
+    ensure => file,
+    content => "$motd",
+  }
+  file { '/home/steam/hlds/tf2/tf/cfg/maplist.txt':
+    ensure => file,
+    content => "ctf_2fort",
+  }
+  file { '/home/steam/hlds/tf2/tf/cfg/mapcycle.txt':
+    ensure => file,
+    content => "ctf_2fort",
+  }
+  file { '/home/steam/hlds/tf2/tf/cfg/ctf_2fort.cfg':
+    ensure => file,
+    content => "mp_timelimit 5"
+  }
+  file { '/home/steam/hlds/tf2/steam_appid.txt':
+    ensure => file,
+    content => "440",
+  }
+  file { '/home/steam/hlds/tf.sh':
+    ensure => file,
+    mode => 764,
+    owner => "steam",
+    content => "#!/bin/sh\ntf2/srcds_run -game tf +sv_pure 1 +map ctf_2fort.bsp +maxplayers 24",
   }
 }
--- tests/init.pp
+++ tests/init.pp
@@ -1,1 +1,7 @@
-include tf2_server
+class { 'tf2_server':
+  hostname => "tf2_server test",
+  rcon_password => "abcd1234",
+  sv_contact => "fake@fake.com",
+  map_timelimit => 15,
+  motd => "Welcome to the tf2_server puppet module test server!",
+}
